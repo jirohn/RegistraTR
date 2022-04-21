2a3f810ea6361df379979469ccd6325091ce6330
jirohn <canariasraptv@gmail.com>
2022-04-21 21:15:02 +0200

    Diseño css de pagina de Config

--- plugin-registratr.php
+++ plugin-registratr.php
@@ -74,35 +74,37 @@ add_action('admin_menu', 'registratr_menu');
 
 function registratr_config_page(){
     echo '
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<h3>
-				<center>RegistraTr settings</center>
-			</h3>
-			<div class="row">
-				<div class="col-md-6">
-                prueba
-				</div>
-				<div class="col-md-6">
-                pruebota
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-                pruebenguel
-				</div>
-				<div class="col-md-6">
-                pruebinotota
-				</div>
-			</div>
-		</div>
-	</div></div>
+   <div class="wrap">
+   <h1>RegistraTr Plugin</h1>
+    <table class="form-table">
+    <tbody><tr>
+        <th scope="row"><label for="recreate">Recreate codes in all users</label></th>
+        <td><input name="recreate" type="button" id="recreate" value="Recreate code" class="regular-text"></td>
+        </tr>
+        <tr>
+        <th scope="row"><label for="sendmailcheck">Send email</label></th>
+        <td><input name="sendmailcheck" type="checkbox" id="sendmailcheck" class="regular-text"></td>
+        </tr>
+        <tr>
+        <th scope="row"><label for="emailforreg">email to registered user</label></th>
+        <td><textarea rows = "5" cols = "50" name="emailforreg" type="textarea" id="emailforreg" value="Write here your message" class="regular-text">
+        </textarea>
+        </td>
+        </tr>
+        <tr>
+        <th scope="row"><label for="emailfornew">email to new user</label></th>
+        <td><textarea rows = "5" cols = "50" name="emailfornew" type="textarea" id="emailfornew" value="Write here your message" class="regular-text">
+        </tr>
+        </textarea>
+        </td>       
+        </tr>
+        
+    </tbody>
+    </table>
+    <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Guardar cambios"></p>
         <form action="" method="post">
-        <p><button type="submit" value="Recrear"/></p>    
-    </body>
+        <p><button type="submit" value="Recrear"/></p>  
+    </div>  
     </html>
 ';
 }
