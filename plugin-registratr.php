a12ab9dbef1932994c8fc98a931be0344f329cf0
jirohn <canariasraptv@gmail.com>
2022-05-16 12:14:49 +0200

    fix anterior 3er intento xD

--- plugin-registratr.php
+++ plugin-registratr.php
@@ -28,11 +28,11 @@ define ('EXTRACTR_PLUGIN_NAME','ExtracTr');
 /**
  * definiendo dependencias // setting dependencies
  */
-include(EXTRACTR_PLUGIN_PATH."/includes/rttr_filters.php");
-include(EXTRACTR_PLUGIN_PATH."/includes/rttr_functions.php");
-include(EXTRACTR_PLUGIN_PATH."/includes/rttr_email_functions.php");
-include(EXTRACTR_PLUGIN_PATH."/public/rttr_user_functions.php");
-include(EXTRACTR_PLUGIN_PATH."/admin/rttr_admin.php");
+include("includes/rttr_filters.php");
+include("includes/rttr_functions.php");
+include("includes/rttr_email_functions.php");
+include("public/rttr_user_functions.php");
+include("admin/rttr_admin.php");
 
 
 
